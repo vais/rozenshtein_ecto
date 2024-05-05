4ddb086a0c1cb5b930c127ebe20425b5b7773841
Vais Salikhov <vsalikhov@gmail.com>
2024-05-04 22:46:00 -0400

    mix new rozenshtein_ecto --sup

---
+++ lib/rozenshtein_ecto.ex
@@ -0,0 +1,18 @@
+defmodule RozenshteinEcto do
+  @moduledoc """
+  Documentation for `RozenshteinEcto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RozenshteinEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rozenshtein_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule RozenshteinEctoTest do
+  use ExUnit.Case
+  doctest RozenshteinEcto
+
+  test "greets the world" do
+    assert RozenshteinEcto.hello() == :world
+  end
+end
