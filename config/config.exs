2c94ce6e01abdf85f56cb6fca9784dca07d3d12b
Vais Salikhov <vsalikhov@gmail.com>
2024-05-08 23:28:04 -0400

    configure the repo to have sql sandbox pool

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :rozenshtein_ecto, RozenshteinEcto.Repo,
   hostname: "localhost",
   database: "rozenshtein_ecto",
   username: "postgres",
-  password: "postgres"
+  password: "postgres",
+  pool: Ecto.Adapters.SQL.Sandbox
 
 config :rozenshtein_ecto, ecto_repos: [RozenshteinEcto.Repo]
