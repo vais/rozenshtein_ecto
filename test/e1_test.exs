dae340e7b002ccc352a4698b89a427e8d67528e2
Vais Salikhov <vsalikhov@gmail.com>
2024-05-09 23:07:57 -0400

    E1 - Who takes CS112?

---
+++ test/e1_test.exs
@@ -0,0 +1,35 @@
+defmodule E1Test do
+  use ExUnit.Case, async: true
+  alias RozenshteinEcto.Repo
+  import Ecto.Query
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+    Repo.insert_all("students", [
+      [sno: "S1"],
+      [sno: "S2"],
+      [sno: "S3"]
+    ])
+
+    Repo.insert_all("takes", [
+      [sno: "S1", cno: "CS112"],
+      [sno: "S2", cno: "CS111"],
+      [sno: "S3", cno: "CS112"]
+    ])
+
+    %{expected: ["S3", "S1"]}
+  end
+
+  describe "E1 - Who takes CS112?" do
+    test "query", %{expected: expected} do
+      query =
+        from t in "takes",
+          where: t.cno == "CS112",
+          select: t.sno,
+          order_by: [desc: t.sno]
+
+      assert Repo.all(query) == expected
+    end
+  end
+end
