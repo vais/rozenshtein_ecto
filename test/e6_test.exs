c3f8c110c5111b12ce0aeb2d9db3a7b0ba4e41c9
Vais Salikhov <vsalikhov@gmail.com>
2024-05-13 22:52:34 -0400

    E6 - Who takes a course which is not CS112?

---
+++ test/e6_test.exs
@@ -0,0 +1,39 @@
+defmodule E6Test do
+  use ExUnit.Case, async: true
+  alias RozenshteinEcto.Repo
+  import Ecto.Query
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+    Repo.insert_all("students", [
+      [sno: "S1"],
+      [sno: "S2"],
+      [sno: "S3"],
+      [sno: "S4"]
+    ])
+
+    Repo.insert_all("takes", [
+      [sno: "S1", cno: "CS112"],
+      [sno: "S1", cno: "CS113"],
+      [sno: "S2", cno: "CS112"],
+      [sno: "S3", cno: "CS111"],
+      [sno: "S3", cno: "CS113"]
+    ])
+
+    %{expected: ["S1", "S3"]}
+  end
+
+  describe "E6 - Who takes a course which is not CS112?" do
+    test "query", %{expected: expected} do
+      query =
+        from t in "takes",
+          where: t.cno != "CS112",
+          select: t.sno,
+          distinct: true
+
+      actual = query |> Repo.all() |> Enum.sort()
+      assert actual == expected
+    end
+  end
+end
