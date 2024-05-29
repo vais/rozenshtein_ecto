86bf34c5e82e34686df95bd00360b8c4278aff6f
Vais Salikhov <vsalikhov@gmail.com>
2024-05-28 22:44:25 -0400

    Which courses are taught by at least two professors?

--- test/composite_keys_test.exs
+++ test/composite_keys_test.exs
@@ -17,7 +17,9 @@ defmodule CompositeKeysTest do
       [fname: "john", lname: "smith", cno: "CS111"],
       [fname: "mary", lname: "smith", cno: "CS112"],
       [fname: "john", lname: "brown", cno: "CS112"],
-      [fname: "mary", lname: "brown", cno: "CS113"]
+      [fname: "mary", lname: "brown", cno: "CS113"],
+      [fname: "mary", lname: "smith", cno: "CS113"],
+      [fname: "john", lname: "brown", cno: "CS113"]
     ])
 
     :ok
@@ -36,6 +38,21 @@ defmodule CompositeKeysTest do
         where: t.cno == "CS112",
         select: fragment("concat(?, ' ', ?)", p.fname, p.lname),
         order_by: [desc: 1],
+        distinct: true
+
+    assert Repo.all(query) == expected
+  end
+
+  test "Which courses are taught by at least two professors?" do
+    expected = ["CS112", "CS113"]
+
+    query =
+      from t1 in "teach",
+        join: t2 in "teach",
+        on: t1.fname == t2.fname and t1.lname == t2.lname,
+        where: t1.cno != t2.cno,
+        select: t1.cno,
+        order_by: t1.cno,
         distinct: true
 
     assert Repo.all(query) == expected
